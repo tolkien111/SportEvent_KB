63df9c8d339694adcc771af9ac930aa9572589dd
Marcin Zygmunt <marcin.zygmunt@pgosa.pl>
2021-10-16 10:40:46 +0200

    Initial commit

---
+++ src/main/java/SportEventsApplication.java
@@ -0,0 +1,37 @@
+import entity.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootApplication
+public class SportEventsApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(SportEventsApplication.class, args);
+        }
+
+        @Component
+        @RequiredArgsConstructor
+        @Profile("dev")
+        static class InitOnStartup {
+
+           private final PersonRepository personRepository;
+           private final EventsRepository eventsRepository;
+
+            @EventListener
+            @Transactional
+            public void setup(ApplicationReadyEvent event) {
+                Player player1 = new Player("Jhon","Doe", LocalDate.of(1990,01,20),"jd","jd@wp.pl","gorlickielwy");
+                Organizator organizator1 = new Organizator("David","Bovie", LocalDate.of(1995,01,20),"jd","jd@wp.pl","TEAMSPORT");
+                personRepository.saveAllAndFlush(List.of(player1));
+            }
+        }
+}
---
+++ src/main/java/entity/EventsRepository.java
@@ -0,0 +1,14 @@
+package entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface EventsRepository extends JpaRepository <Event, UUID> {
+
+    List<Event> findByEventName (String eventName);
+    List<Event> findByOrganizator (Organizator organizator);
+    List<Event> findByEventDate (LocalDateTime eventDate);
+}
